ea01311413ca0cc407fc05244e4554b2e167375b
Francois Corthay <francois.corthay@gmail.com>
2024-09-26 14:45:41 +0200

    enriched command line parameter handling for radio streaming script

--- Management/streamRadio.bash
+++ Management/streamRadio.bash
@@ -2,31 +2,80 @@
 
 AUDIO_BASE_DIR=$(dirname $(dirname $0))
 INDENT='  '
-                                                         # command line argument
-command=${1:-list}
-command=`echo $command | tr '[:upper:]' '[:lower:]'`
 
+# ------------------------------------------------------------------------------
+# Command line arguments
+#
+                                                                # default values
+listStations=false
+station=''
+endSreaming=false
+verbose=false
+                                                             # specify arguments
+arguments='ls:evh'
+declare -A longArguments
+longArguments=(
+  ["l"]="listStations" ["s"]="station" ["e"]="endSreaming"
+  ["v"]="verbose"
+)
+                                                             # show script usage
+usage() {
+  echo "Usage: $(basename "$0") [-l|--listStations] [-s|--station station]" 1>&2 
+  echo "${INDENT}[-e|--endSreaming] [-v|--verbose]" 1>&2 
+  exit
+}
+                                                        # replace long arguments
+if [ $# -gt 0 ] ; then
+  for index in $(eval echo "{1..${#}}") ; do
+    for argument in "${!longArguments[@]}" ; do
+      if [ ${!index} = "--${longArguments[$argument]}" ] ; then
+        set -- "${@:1:((index - 1))}" "-$argument" "${@:((index + 1)):${#}}"
+      fi
+    done
+  done
+fi
+                                                               # parse arguments
+while getopts ${arguments} option; do
+  case ${option} in
+    l) listStations=true ;;
+    s) station="$OPTARG" ;;
+    e) endSreaming=true ;;
+    v) verbose=true ;;
+    h) usage ;;
+    ?) usage
+  esac
+done
+shift $((OPTIND-1))
+station=`echo $station | tr '[:upper:]' '[:lower:]'`
+
+if [ -z $station ] && [ $endSreaming = 'false' ] ; then
+  listStations=true
+fi
+
+# ------------------------------------------------------------------------------
+# Main script
+#
                                                         # configuration elements
 source $AUDIO_BASE_DIR/configuration.bash
 AUDIO_SINK="dmix:$ALSA_LOOPBACK_CAPTURE_DEVICE,$SNAPSERVER_LOOPBACK_SUBDEVICE"
 STATION_LIST=(`env | grep 'RADIO_'`)
                                                        # get radio stations info
 declare -A stationDictionary
-  for station in ${STATION_LIST[@]} ; do
-    name=`echo $station | cut -d '=' -f 1`
-    url=`echo $station | cut -d '=' -f 2`
+  for knownStation in ${STATION_LIST[@]} ; do
+    name=`echo $knownStation | cut -d '=' -f 1`
+    url=`echo $knownStation | cut -d '=' -f 2`
     name=${name#RADIO_}
     name=`echo $name | tr '[:upper:]' '[:lower:]'`
     stationDictionary[$name]=$url
   done
                                                            # list radio stations
-if [ "$command" = 'list' ] ; then
+if [ $listStations = 'true' ] ; then
   echo "Known radio stations:"
   for station in ${!stationDictionary[@]} ; do
     echo "$INDENT$station : ${stationDictionary[$station]}"
   done
                                                           # stop radio streaming
-elif [ "$command" = 'stop' ] ; then
+elif [ $endSreaming = 'true' ] ; then
   IFS=$'\n' presentStreams=(`ps ax | grep -v grep | grep vlc`)
   for presentStream in "${presentStreams[@]}" ; do
     presentStream=`echo $presentStream | sed 's/^\s*//'`
@@ -38,18 +87,20 @@ elif [ "$command" = 'stop' ] ; then
   $AUDIO_BASE_DIR/Mopidy/control.bash play
                                                                   # stream radio
 else
-  echo "Streaming radio from $command"
                                                                       # find URL
   streamUrl=''
-  for station in ${!stationDictionary[@]} ; do
-    stationShort=${station:0:${#command}}
-    commandCompare=${command// /_}
-    if [ "$commandCompare" = "$stationShort" ] ; then
-      streamUrl=${stationDictionary[$station]}
+  stationName=$station
+  for knownStation in ${!stationDictionary[@]} ; do
+    stationShort=${knownStation:0:${#station}}
+    stationCompare=${station// /_}
+    if [ "$stationCompare" = "$stationShort" ] ; then
+      streamUrl=${stationDictionary[$knownStation]}
+      stationName=$knownStation
     fi
   done
+  echo "Streaming radio from $stationName"
   if [ -z $streamUrl ] ; then
-      echo "$INDENT\"$command\" not found"
+      echo "${INDENT}station \"$station\" not found"
       exit
   fi
                                                     # check if already streaming
@@ -62,11 +113,15 @@ else
     fi
   fi
   if [ -n "$streamProcessId" ] ; then
-    echo "${INDENT}Stopping $currentStreamUrl"
+    if [ $verbose = 'true' ] ; then
+      echo "${INDENT}Stopping $currentStreamUrl"
+    fi
     kill $streamProcessId
   fi
                                                             # launching streamer
-  echo "${INDENT}Streaming from $streamUrl"
+  if [ $verbose = 'true' ] ; then
+    echo "${INDENT}Streaming from $streamUrl"
+  fi
   $AUDIO_BASE_DIR/Mopidy/control.bash pause
   cvlc -A alsa --alsa-audio-device $AUDIO_SINK $streamUrl > /dev/null 2>&1 &
 fi
