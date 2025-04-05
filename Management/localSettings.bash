81f3f388b14056e9fce6c2a093ded2a35da3882a
Francois Corthay <francois.corthay@gmail.com>
2025-04-05 17:11:35 +0200

    added backup and restore for local configuration files

---
+++ Management/localSettings.bash
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+BASE_DIRECTORY=`dirname $0`
+BASE_DIRECTORY=`realpath $BASE_DIRECTORY/..`
+BACKUP_DIRECTORY="$BASE_DIRECTORY/LocalSettings"
+LOCAL_FILES=(
+  'configuration.bash'
+  'Management/localSettings.bash'
+)
+INDENT='  '
+                                                                  # check action
+action=$1
+if [ -z "$action" ] ; then
+  echo "Use \"localSettings.bash backup\" or \"localSettings.bash restore\""
+  exit 1
+fi
+                                                                  # backup files
+if [ "$action" = 'backup' ]; then
+  echo "Backing-up local settings to \"$BACKUP_DIRECTORY\""
+  mkdir -p $BACKUP_DIRECTORY
+  for localFile in "${LOCAL_FILES[@]}" ; do
+     echo "$INDENT$localFile"
+     pathlessFile=`basename $localFile`
+     cp $BASE_DIRECTORY/$localFile $BACKUP_DIRECTORY/$pathlessFile
+  done
+elif [ "$action" = 'restore' ]; then
+  echo "Restoring local settings from \"$BACKUP_DIRECTORY\""
+  for localFile in "${LOCAL_FILES[@]}" ; do
+     echo "$INDENT$localFile"
+     pathlessFile=`basename $localFile`
+     cp $BACKUP_DIRECTORY/$pathlessFile $BASE_DIRECTORY/$localFile
+  done
+else
+  exit 2
+fi
--- configuration.bash
+++ configuration.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+# scripts base directory
+export AUDIO_BASE_DIR=$(dirname "${BASH_SOURCE[0]}")
+
 # multiroom environment
 export MULTIROOM_SNAPCAST_SERVER='Persephone.local'
 
@@ -10,10 +13,10 @@ export AUDIO_CHANNEL_NB=2
 export AUDIO_RATE=192000
 
 # ALSA loopback channels
+export CAMILLADSP_LOOPBACK_SUBDEVICE='0'
+export SNAPSERVER_LOOPBACK_SUBDEVICE='1'
 export ALSA_LOOPBACK_CAPTURE_DEVICE='Loopback,0'
 export ALSA_LOOPBACK_PLAYBACK_DEVICE='Loopback,1'
-export SNAPSERVER_LOOPBACK_SUBDEVICE='0'
-export CAMILLADSP_LOOPBACK_SUBDEVICE='1'
 
 # CamillaDSP
 export CAMILLA_DIR=`realpath -s ~/Documents/RPi-multiroom-audio/CamillaDSP/`
@@ -24,10 +27,3 @@ export CAMILLA_COEFFICIENTS_DIR="$CAMILLA_CONFIGURATION_DIR/Coefficients"
 export CAMILLA_BACKEND_DIR="$CAMILLA_DIR/Backend"
 export CAMILLA_GUI_STATE_FILE="$CAMILLA_BACKEND_DIR/statefile.yml"
 export CAMILLA_GUI_PORT='5005'
-
-# radio stations
-export RADIO_COULEUR_3='http://stream.srg-ssr.ch/m/couleur3/mp3_128'
-export RADIO_OPTION_MUSIQUE='http://stream.srg-ssr.ch/m/option-musique/mp3_128'
-export RADIO_SWISS_POP='http://stream.srg-ssr.ch/m/rsp/mp3_128'
-export RADIO_ROCK_BALLADS='http://spoonradiorockballads.ice.infomaniak.ch/spoon-rockballads-hd.aac'
-export RADIO_NOSTALGIE='http://cdn.nrjaudio.fm/adwz2/fr/30601/mp3_128.mp3'
